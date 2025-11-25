6ccea992394a24171bc3f92819505887cadde130
Nikunj-52147 <nikunj51260@gmail.com>
2025-11-25 10:07:35 +0530

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+        <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+        <ul>
+        <li><img src={mail_icon} alt="" />Contact@Nikunj.dev</li>
+        <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
+        <li><img src={location_icon} alt="" />77 Massachusetts Ave, Cambridge <br />MA 02139, United States</li>
+      </ul>
+      </div>
+      
+      <div className="contact-col">
+        <form action="">
+            <label htmlFor="name">Your name</label>
+            <input type="text" name="name" id="name" placeholder='Enter your name' required />
+            <label htmlFor="phone">Phone Number</label>
+            <input type="tel" name="phone" id="phone" placeholder='Enter your mobile number' required />
+            <label htmlFor="">Write your message here</label>
+            <textarea name="message" id="" rows="6" placeholder='Enter your message' required></textarea>
+            <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
+        </form>
+        {/* <span>sending</span> */}
+      </div>
+    </div>
+  )
+}
+
+export default Contact
